d8ea07539fe20e0aa8c6eeda0ac7484b539c05ea
Vivek Kumar <clocktower77@protonmail.com>
2024-03-20 00:34:58 +0530

    add resolver and typedefs

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 import {startStandaloneServer} from "@apollo/server/standalone"
 
+
+import { typeDefs }  from "../schema";
+import _db from "./_db";
 // Server setup
+
+const resolvers = {
+    Query : {
+        games () {
+            return _db.games
+        },
+        reviews(){
+            return _db.reviews
+        },
+        authors(){
+            return _db.authors
+        }
+    }
+}
 
 
 const server = new ApolloServer({
     // typeDef --definations of types of data
-    //resolvers
+    //resolvers--how we respond to the queries 
+    typeDefs,
+    resolvers
 
 })
 
