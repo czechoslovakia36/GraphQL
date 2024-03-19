5e816a53ef97f5cb13922b9907e1cb3aacc1fe2f
Kumar,Vivek <clocktower77@protonmail.com>
2024-03-19 23:01:54 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone"
+
+// Server setup
+
+
+const server = new ApolloServer({
+
+})
+
+const { url } = await startStandaloneServer(server,{
+    listen: {port:4000}
+})
+
+
+console.log("Server reay at port",4000)
